d437f40fe8a7dacf82c8e4896a739614fc50d7b3
NeikoStream <71373738+NeikoStream@users.noreply.github.com>
2023-04-07 23:34:38 +0200

    v0

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>CraftStat</title>
+</head>
+<body>
+    <table class="table table-bordered">
+        <thead class="table-dark">
+            <tr>
+                <th>Nom</th>
+                <th>Temps de jeu</th>
+                <th>Morts</th>
+                <th>Terre</th>
+            </tr>
+        </thead>
+        <tbody>
+            <td>Steve</td>
+            <td>999 jours</td>
+            <td>0</td>
+            <td>99999</td>
+        </tbody>
+        <tbody>
+            <td>Steve</td>
+            <td>999 jours</td>
+            <td>0</td>
+            <td>99999</td>
+        </tbody>
+        <tbody>
+            <td>Steve</td>
+            <td>999 jours</td>
+            <td>0</td>
+            <td>99999</td>
+        </tbody>
+        <tbody>
+            <td>Steve</td>
+            <td>999 jours</td>
+            <td>0</td>
+            <td>99999</td>
+        </tbody>
+    </table>
+</body>
+</html>
