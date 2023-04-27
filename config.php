e612c6f38f4ba86757fba7cf2db4e9fd39b04bb1
Nicolas Rousseau <71373738+NeikoStream@users.noreply.github.com>
2023-04-27 13:41:34 +0200

    DEBUG !!!!!

--- config.php
+++ config.php
@@ -6,10 +6,8 @@ $db_password = 'passwordDataBase';
 $db_name = 'nameDataBase';
 $db_host = '0.0.0.0:3306'; //adresse de la BD
 
-
 //Config general
 $serverName = "Nom du serveur";
-$bluemap = "http://adresse de la carte dynmap ou blue map/";
+$bluemap = "https://adresse de la carte dynmap ou blue map/";
 $ip = "ip du serveur complete";
-
-?>
+$port = "25565"; // port du serveur
--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<?php 
-require_once('config.php');
-require_once('src/fonction/fonction.php');
+<?php
+require_once 'config.php';
+require_once 'src/fonction/fonction.php';
 
-//Connexion a la bd
 try {
     $linkpdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_username, $db_password);
 } catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage()); 
+    die('Erreur : ' . $e->getMessage());
 }
+
 //Récupérer la table stat avec toutes les infos
 
 $query = "SELECT player_name, TOTAL_WORLD_TIME, DEATHS, (IFNULL(JSON_EXTRACT(mine_block, '$.DIAMOND_ORE'),0) + IFNULL(JSON_EXTRACT(mine_block, '$.DEEPSLATE_DIAMOND_ORE'),0)) as diamants FROM stats";
 $params = [];
 //fonction de recherche
-if(!empty($_GET['q'])){
+if (!empty($_GET['q'])) {
     $query .= " WHERE player_name LIKE :pseudo";
     $params['pseudo'] = '%' . $_GET['q'] . '%';
 }
@@ -21,24 +21,23 @@ if(!empty($_GET['q'])){
 $query .= " ORDER BY TOTAL_WORLD_TIME DESC";
 $players = $linkpdo->prepare($query);
 
-$players -> execute($params);
-$players = $players -> fetchALL();
-
+$players->execute($params);
+$players = $players->fetchALL();
 
 //Podium Temps
 $toptime = $linkpdo->prepare("SELECT player_name, TOTAL_WORLD_TIME FROM stats ORDER BY TOTAL_WORLD_TIME DESC LIMIT 3");
-$toptime -> execute();
-$toptime = $toptime -> fetchALL();
+$toptime->execute();
+$toptime = $toptime->fetchALL();
 
 //Podium Morts
 $topDead = $linkpdo->prepare("SELECT player_name, DEATHS FROM stats ORDER BY DEATHS DESC LIMIT 3");
-$topDead -> execute();
-$topDead = $topDead -> fetchALL();
+$topDead->execute();
+$topDead = $topDead->fetchALL();
 
 //Podium Diamants
 $topDiamant = $linkpdo->prepare("SELECT player_name, (IFNULL(JSON_EXTRACT(mine_block, '$.DIAMOND_ORE'),0) + IFNULL(JSON_EXTRACT(mine_block, '$.DEEPSLATE_DIAMOND_ORE'),0)) as diamants FROM stats ORDER BY diamants DESC LIMIT 3");
-$topDiamant -> execute();
-$topDiamant = $topDiamant -> fetchALL();
+$topDiamant->execute();
+$topDiamant = $topDiamant->fetchALL();
 ?>
 
 
@@ -57,13 +56,17 @@ $topDiamant = $topDiamant -> fetchALL();
     <div class="container text-center">
         <div class="row">
             <div class="col">
-                <?php
-                    $api = json_decode(file_get_contents("https://api.mcstatus.io/v2/status/java/".$ip));
-                    $online = $api->players->online;
-                    for ($i=0; $i < $online; $i++) { 
-                        $playersOnline[$i] = $api->players->list[$i]->name_clean;
-                    }
-                ?>
+               <?php
+$api = json_decode(file_get_contents("https://api.mcstatus.io/v2/status/java/" . $ip . ":" . $port));
+$online = $api->players->online;
+if ($online !== 0) {
+    for ($i = 0; $i < $online; $i++) {
+        $playersOnline[$i] = $api->players->list[$i]->name_clean;
+    }
+
+}
+
+?>
 
                     <h1><?=$online?> en ligne</h1>
                     <h2></h2>
@@ -73,8 +76,8 @@ $topDiamant = $topDiamant -> fetchALL();
             </div>
             <div class="col">
                 <form action="" class="p-2">
-                        <div class="form-group hstack">                     
-                            <input type="text" class="form-control" name="q" placeholder="Rechercher un pseudo" value="<?php if(isset($_GET['q'])){echo htmlentities($_GET['q']);} ?>">
+                        <div class="form-group hstack">
+                            <input type="text" class="form-control" name="q" placeholder="Rechercher un pseudo" value="<?php if (isset($_GET['q'])) {echo htmlentities($_GET['q']);}?>">
                             <button class="btn btn-primary mx-2">🔎</button>
                         </div>
                 </form>
@@ -92,22 +95,30 @@ $topDiamant = $topDiamant -> fetchALL();
         </thead>
         <tbody>
             <?php
-            foreach($players as $player):
-            ?>
-            <tr class="<?php if (in_array($player['player_name'],$playersOnline)) {
-                echo "EnLigne";
-            }?>">
+foreach ($players as $player):
+?>
+
+            <tr class="<?php
+if ($online !== 0) {
+    if (in_array($player['player_name'], $playersOnline)) {
+        echo "EnLigne";
+    }
+}?>
+
+            ">
                 <td>
-                    <img src="https://minotar.net/avatar/<?=$player['player_name']?>/32.png" alt="image du joueur"> <?=$player['player_name']?> 
-                    <?php if (in_array($player['player_name'],$playersOnline)) {?>
+                    <img src="https://minotar.net/avatar/<?=$player['player_name']?>/32.png" alt="image du joueur"> <?=$player['player_name']?>
+                    <?php if ($online !== 0) {
+    if (in_array($player['player_name'], $playersOnline)) {?>
                         <img src="src/img/online.png" alt="enLigne" height="30px" class="ps-2">
-                    <?php } ?>
+                    <?php }
+}?>
                 </td>
                 <td><?=TickToTime($player['TOTAL_WORLD_TIME'])?></td>
                 <td><?=$player['DEATHS']?></td>
                 <td><?=$player['diamants']?></td>
             </tr>
-            <?php endforeach ?>
+            <?php endforeach?>
         </tbody>
     </table>
 
@@ -120,26 +131,26 @@ $topDiamant = $topDiamant -> fetchALL();
                     <div class="left vstack">
                         <div class="pt-2">
                             <img src="https://minotar.net/avatar/<?=$toptime[0]['player_name'] ?? "Notch"?>/32.png" alt="image du joueur">
-                            <b><?=$toptime[0]['player_name'] ?? "Personne"?></b> 
-                            <b><?=TickToTimeShort($toptime[0]['TOTAL_WORLD_TIME'])?></b> 
+                            <b><?=$toptime[0]['player_name'] ?? "Personne"?></b>
+                            <b><?=TickToTimeShort($toptime[0]['TOTAL_WORLD_TIME'])?></b>
                         </div>
                     </div>
 
                     <div class="top">
                         <div class="pt-2">
                             <img src="https://minotar.net/avatar/<?=$toptime[1]['player_name'] ?? "Notch"?>/32.png" alt="image du joueur">
-                            <b><?=$toptime[1]['player_name'] ?? "Personne"?></b> 
+                            <b><?=$toptime[1]['player_name'] ?? "Personne"?></b>
                             <br>
-                            <b><?=TickToTimeShort($toptime[1]['TOTAL_WORLD_TIME'] ?? 0)?></b> 
+                            <b><?=TickToTimeShort($toptime[1]['TOTAL_WORLD_TIME'] ?? 0)?></b>
                         </div>
                     </div>
 
                     <div class="right">
                         <div class="pt-2">
                             <img src="https://minotar.net/avatar/<?=$toptime[2]['player_name'] ?? "Notch"?>/32.png" alt="image du joueur">
-                            <b><?=$toptime[2]['player_name'] ?? "Personne"?></b> 
+                            <b><?=$toptime[2]['player_name'] ?? "Personne"?></b>
                             <br>
-                            <b><?=TickToTimeShort($toptime[2]['TOTAL_WORLD_TIME'] ?? 0)?></b> 
+                            <b><?=TickToTimeShort($toptime[2]['TOTAL_WORLD_TIME'] ?? 0)?></b>
                         </div>
                     </div>
                 </div>
@@ -150,26 +161,26 @@ $topDiamant = $topDiamant -> fetchALL();
                     <div class="left vstack">
                         <div class="pt-2">
                             <img src="https://minotar.net/avatar/<?=$topDead[0]['player_name'] ?? "Notch"?>/32.png" alt="image du joueur">
-                            <b><?=$topDead[0]['player_name'] ?? "Personne"?></b> 
-                            <b><?=$topDead[0]['DEATHS']?>☠</b> 
+                            <b><?=$topDead[0]['player_name'] ?? "Personne"?></b>
+                            <b><?=$topDead[0]['DEATHS']?>☠</b>
                         </div>
                     </div>
 
                     <div class="top">
                         <div class="pt-2">
                             <img src="https://minotar.net/avatar/<?=$topDead[1]['player_name'] ?? "Notch"?>/32.png" alt="image du joueur">
-                            <b><?=$topDead[1]['player_name'] ?? "Personne"?></b> 
+                            <b><?=$topDead[1]['player_name'] ?? "Personne"?></b>
                             <br>
-                            <b><?=$topDead[1]['DEATHS'] ?? 0?>☠</b> 
+                            <b><?=$topDead[1]['DEATHS'] ?? 0?>☠</b>
                         </div>
                     </div>
 
                     <div class="right">
                         <div class="pt-2">
                             <img src="https://minotar.net/avatar/<?=$topDead[2]['player_name'] ?? "Notch"?>/32.png" alt="image du joueur">
-                            <b><?=$topDead[2]['player_name'] ?? "Personne"?></b> 
+                            <b><?=$topDead[2]['player_name'] ?? "Personne"?></b>
                             <br>
-                            <b><?=$topDead[2]['DEATHS'] ?? 0?>☠</b> 
+                            <b><?=$topDead[2]['DEATHS'] ?? 0?>☠</b>
                         </div>
                     </div>
                 </div>
@@ -180,26 +191,26 @@ $topDiamant = $topDiamant -> fetchALL();
                     <div class="left vstack">
                         <div class="pt-2">
                             <img src="https://minotar.net/avatar/<?=$topDiamant[0]['player_name'] ?? "Notch"?>/32.png" alt="image du joueur">
-                            <b><?=$topDiamant[0]['player_name'] ?? "Personne"?></b> 
-                            <b><?=($topDiamant[0]['diamants'])?>💎</b> 
+                            <b><?=$topDiamant[0]['player_name'] ?? "Personne"?></b>
+                            <b><?=($topDiamant[0]['diamants'])?>💎</b>
                         </div>
                     </div>
 
                     <div class="top">
                         <div class="pt-2">
                             <img src="https://minotar.net/avatar/<?=$topDiamant[1]['player_name'] ?? "Notch"?>/32.png" alt="image du joueur">
-                            <b><?=$topDiamant[1]['player_name'] ?? "Personne"?></b> 
+                            <b><?=$topDiamant[1]['player_name'] ?? "Personne"?></b>
                             <br>
-                            <b><?=($topDiamant[1]['diamants']) ?? 0?>💎</b> 
+                            <b><?=($topDiamant[1]['diamants']) ?? 0?>💎</b>
                         </div>
                     </div>
 
                     <div class="right">
                         <div class="pt-2">
                             <img src="https://minotar.net/avatar/<?=$topDiamant[2]['player_name'] ?? "Notch"?>/32.png" alt="image du joueur">
-                            <b><?=$topDiamant[2]['player_name'] ?? "Personne"?></b> 
+                            <b><?=$topDiamant[2]['player_name'] ?? "Personne"?></b>
                             <br>
-                            <b><?=($topDiamant[2]['diamants']) ?? 0?>💎</b> 
+                            <b><?=($topDiamant[2]['diamants']) ?? 0?>💎</b>
                         </div>
                     </div>
                 </div>
@@ -211,6 +222,6 @@ $topDiamant = $topDiamant -> fetchALL();
             <a href="http://57.128.22.208:40008/"><img src="src/img/carte.png" id="carte" alt="image d'une carte"></a>
         </div>
     </div>
-    
+
 </body>
 </html>
